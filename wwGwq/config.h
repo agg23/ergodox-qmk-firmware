88c3a9a0a11b0c8b2515cfe1f0612aee50ed055f
Adam Gastineau <adam@agg.im>
2025-02-17 07:30:23 -0800

    Attempted adding suspend delay

--- wwGwq/config.h
+++ wwGwq/config.h
@@ -3,7 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define USB_SUSPEND_WAKEUP_DELAY 0
+#define USB_SUSPEND_WAKEUP_DELAY 3000
 #define SERIAL_NUMBER "wwGwq/7awEo"
 #define LAYER_STATE_8BIT
 
